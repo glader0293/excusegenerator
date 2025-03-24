0d69ec6ead8e7eaadb1972baebca8fd1dd84942e
Andrew Matsumura <42308941+glader0293@users.noreply.github.com>
2025-03-24 05:57:22 +0000

    Final Version

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+    const excuseSentence = () =>{
+      let finalSentence = [];
+      const randomNumberWho = Math.floor(Math.random() * who.length);
+      const randomNumberAction = Math.floor(Math.random() * action.length);
+      const randomNumberWhat = Math.floor(Math.random() * what.length);
+      const randomNumberWhen = Math.floor(Math.random() * when.length) ;
+      
+      let whoPush = finalSentence.push(who[randomNumberWho])
+      let actionPush = finalSentence.push(action[randomNumberAction])
+      let whatPush = finalSentence.push(what[randomNumberWhat])
+      let whenPush = finalSentence.push(when[randomNumberWhen])
+      
+      let joinedSentence = finalSentence.join(" ")
+      document.getElementById("excuse").innerHTML = joinedSentence;
+      
+      }
+  excuseSentence();
 };
+
+
